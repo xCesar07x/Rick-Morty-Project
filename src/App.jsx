fd9a7128ad85cc46513a9d750ae375656842c9de
xCesar07x <mathcesar02@gmail.com>
2023-02-12 14:57:45 -0500

    page-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import image from './assets/image.svg'
+import image from './assets/morty1080p.jpg'
+import rickandmorty from './assets/loading.png'
 import './App.css'
 import axios from 'axios'
 // import ResidentInfo from './ResidentInfo'
@@ -10,77 +11,91 @@ function App() {
 
   const [searchId, setSearchId] = useState("")
 
-  
+  const [isLoading, setIsloading] = useState(true);
+
+
 
   useEffect(() => {
     const randomId = Math.floor(Math.random() * 127);
-    axios.get(`https://rickandmortyapi.com/api/location/${randomId}`) 
-    .then(res => setLocation(res.data)); 
+    axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
+      .then(res => {
+        setLocation(res.data)
+        setTimeout(() => setIsloading(false), 2000)
+      });
 
   }, [])
 
-  console.log(location)
+  // console.log(location)
 
   const searchType = () => {
     axios.get(`https://rickandmortyapi.com/api/location/${searchId}`)
-    .then(res => setLocation(res.data));
+      .then(res => setLocation(res.data));
   }
 
   return (
     <div className="App">
-      
-      <header >
-        <img className='img-header' src={image} alt="" />
-    
-      </header>
-      
-      
-      
+      {
+        isLoading ? (
+          <div className='loading-icon'><img className='rnm-img' src={rickandmorty} alt="" /></div>
 
-      
-      <div className='conteiner'>
-      
-      <div className='input-conteiner'>
-      <input className='input'
-      type="text" 
-      placeholder='type a pokemon type id'
-      value={searchId}
-      onChange = {e => setSearchId(e.target.value)}
-      
-      />
-        
-      
-      <button className='button'
-       onClick={searchType}
-      >
-        Search
-      </button>
-      </div>
-      
-      <div className='ubication'>
-      <h2>{location.name}</h2>
-      <ul className='list-location'>
-        <li><p>Type:</p> {location.type}</li>
-        <li><p>Dimension:</p> {location.dimension}</li>
-        <li><p>Population:</p> {location.residents?.length}</li>
-      </ul>
-      </div>
+        ) : (
+          <>
+            <header className="header">
 
-      <h3 className='residents'>Residents</h3>
+              <img className='img-header' src={image} alt="" />
 
-      </div>
-      <ul className='location'>
-        
-        {
-          location.residents?.map(location => (
-            <LocationInfo location={location} key={location}/>
-          ))
-        }
-        
-      </ul>
-      
-      
-    </div>
+            </header>
+
+
+
+
+
+            <div className='conteiner'>
+
+              <div className='input-conteiner'>
+                <input className='input'
+                  type="text"
+                  placeholder='type a location type id'
+                  value={searchId}
+                  onChange={e => setSearchId(e.target.value)}
+
+                />
+
+
+                <button className='button'
+                  onClick={searchType}
+                >
+                  Search
+                </button>
+              </div>
+
+              <div className='ubication'>
+                <p>Planet:</p>
+                <h2>{location.name}</h2>
+                <ul className='list-location'>
+                  <li><p>Type:</p> {location.type}</li>
+                  <li><p>Dimension:</p> {location.dimension}</li>
+                  <li><p>Population:</p> {location.residents?.length}</li>
+                </ul>
+              </div>
+
+              <h3 className='residents'>Residents</h3>
+
+            </div>
+            <ul className='location'>
+
+              {
+                location.residents?.map(location => (
+                  <LocationInfo location={location} key={location} />
+                ))
+              }
+
+            </ul>
+          </>
+        )
+      }
+
+    </div >
   )
 }
 
--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 const LocationInfo = ({location}) => {
 
     const[resident, setResident] = useState({});
+    
 
     useEffect(() => {
         axios.get(location)
@@ -12,15 +13,37 @@ const LocationInfo = ({location}) => {
     }, [])
 
     // console.log(resident)
+
+    let colorDot = '';
+
+    if (resident.status === 'Alive') {
+        colorDot = 'green'
+    } if (resident.status === 'Dead') {
+        colorDot = 'red'
+    } if (resident.status === 'unknown'){
+        colorDot = 'gray'
+    }
+        
+    
+
     return (
         <div className='col'>
             <div className='resident-card'>
             
              <img src={resident.image} alt="" /> <br />
-            
+              
+             <div className='status'>
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={colorDot} className="bi bi-circle-fill" viewBox="0 0 16 16"> <circle cx="8" cy="8" r="8"/> </svg>
+             <div>
+             <p>Status:</p>  {resident.status} 
+             </div>
+              </div>
+
              <div className='resident-info'>
-             {resident.name} <br />
-             <p>Status:</p> {resident.status} <br />
+
+             
+
+             <p>Name:</p>{resident.name} <br />
              <p>Origin:</p>{resident.origin?.name} <br />
              <p>Episode appearances:</p>{resident.episode?.length} <br />
              </div>
